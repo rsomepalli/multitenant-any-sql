b4cd51f211296def8f4831ed617353a202a4b84b
Ravi Somepalli <ravi.somepalli@ticketfly.com>
2017-05-30 21:46:19 -0700

    add mysql data source for single db instance

---
+++ mysql/src/main/java/com/analogyx/samples/mysql/MysqlSimpleTenantAwareDataSource.java
@@ -0,0 +1,16 @@
+package com.analogyx.samples.mysql;
+
+import javax.sql.DataSource;
+
+public class MysqlSimpleTenantAwareDataSource extends MysqlTenantAwareDataSource {
+
+	public MysqlSimpleTenantAwareDataSource(){
+		this.setTenantDBInstanceProvider(tenant->DEFAULT);
+	}
+
+	private static final String DEFAULT="default";
+	
+	public void setDataSource(DataSource ds){
+		this.addDbInstance(DEFAULT, ds);
+	}
+}
